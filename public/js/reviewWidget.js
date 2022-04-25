ffe6100ec5f25246bd957390bac707e3dde552fb
rob@bonzaballoons.co.uk <rob@bonzaballoons.co.uk>
2022-04-25 18:46:56 +0200

    changes

--- public/js/reviewWidget.js
+++ public/js/reviewWidget.js
@@ -1,14 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**************************************!*\
-  !*** ./resources/js/reviewWidget.js ***!
-  \**************************************/
-richSnippetReviewsWidgets('vertical-widget-360', {
-  store: 'balloonprinting-co-uk',
-  widgetName: 'vertical',
-  primaryClr: '#f47e27',
-  layout: 'fullWidth',
-  numReviews: 1
-});
-/******/ })()
-;
+richSnippetReviewsWidgets("vertical-widget-360",{store:"balloonprinting-co-uk",widgetName:"vertical",primaryClr:"#f47e27",layout:"fullWidth",numReviews:1});
