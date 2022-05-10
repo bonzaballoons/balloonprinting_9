9dceadac3ac27562b5cdcbab15c3f50968168511
rob@bonzaballoons.co.uk <rob@bonzaballoons.co.uk>
2022-05-10 13:19:07 +0200

    mailgun fix

--- config/mail.php
+++ config/mail.php
@@ -49,7 +49,9 @@ return [
         ],
 
         'mailgun' => [
-            'transport' => 'mailgun',
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.eu.mailgun.net'),
         ],
 
         'postmark' => [
