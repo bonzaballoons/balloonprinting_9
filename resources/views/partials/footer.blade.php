9cfb51c0e7a87190765914880e0255995439efc0
bonzaballoons <rob@bonzaballoons.co.uk>
2022-10-07 11:09:37 +0200

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -89,7 +89,7 @@
     </div>
 </footer>
 <div class="alert text-center cookiealert" role="alert">
-    <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you get the best experience on our website. <a href="{{ url('main/privacy') }}" target="_blank">Learn more</a>
+    <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you get the best experience on our website. <a href="{{ url('privacy') }}" target="_blank">Learn more</a>
 
     <button type="button" class="btn btn-primary btn-sm acceptcookies">
         I agree
