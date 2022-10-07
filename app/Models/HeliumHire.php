1b93d48d0d6961f40d4012f682d7b8a4809bc275
bonzaballoons <rob@bonzaballoons.co.uk>
2022-10-07 09:34:02 +0200

    update data helium for price page

--- app/Models/HeliumHire.php
+++ app/Models/HeliumHire.php
@@ -14,16 +14,23 @@ class HeliumHire extends Product
             });
         });
     }
-
-    private static $balloonsCanFill = [ 250, 310, 500, 620, 935, 1250 ];
-    private static $miniDescriptions = [
-        'Our most popular option! Perfect for home use, party occasions and small events, our light, easy to use hire cylinder offers great value for money.',
-        'A great choice for small events, this fantastic lightweight cylinder is so easy to use and perfect if you need to transport it to your venue.',
-        'An ideal choice for medium sized events and special occasions, this hired helium cylinder gives real value for money for that important event',
-        'The perfect choice if you want to inflate large numbers of balloons, this lightweight and easy to use cylinder is great for decorating venues and taking to events.',
-        'A great value option for corporate events or promotions, this cylinder will make inflating large numbers of balloons a doddle!',
-        'A huge cylinder giving the most cost effective price per balloon solution, ideal for large scale corporate, promotional events & in retail outlets.'
+    
+   private static $balloonDetails = [
+        [ 'id' => 4876, 'balloonsCanFill' => 250, 'miniDesc' => 'Our most popular option! Perfect for home use, party occasions and small events, our light, easy to use hire cylinder offers great value for money.'],
+        [ 'id' => 6093, 'balloonsCanFill' => 310, 'miniDesc' => 'A great choice for small events, this fantastic lightweight cylinder is so easy to use and perfect if you need to transport it to your venue.'],
+        [ 'id' => 6094, 'balloonsCanFill' => 620, 'miniDesc' => 'The perfect choice if you want to inflate large numbers of balloons, this lightweight and easy to use cylinder is great for decorating venues and taking to events.'],
+        [ 'id' => 6095, 'balloonsCanFill' => 935, 'miniDesc' => 'A great value option for corporate events or promotions, this cylinder will make inflating large numbers of balloons a doddle!'],
     ];
+
+//     private static $balloonsCanFill = [ 250, 310, 500, 620, 935, 1250 ];
+//     private static $miniDescriptions = [
+//         'Our most popular option! Perfect for home use, party occasions and small events, our light, easy to use hire cylinder offers great value for money.',
+//         'A great choice for small events, this fantastic lightweight cylinder is so easy to use and perfect if you need to transport it to your venue.',
+//         'An ideal choice for medium sized events and special occasions, this hired helium cylinder gives real value for money for that important event',
+//         'The perfect choice if you want to inflate large numbers of balloons, this lightweight and easy to use cylinder is great for decorating venues and taking to events.',
+//         'A great value option for corporate events or promotions, this cylinder will make inflating large numbers of balloons a doddle!',
+//         'A huge cylinder giving the most cost effective price per balloon solution, ideal for large scale corporate, promotional events & in retail outlets.'
+//     ];
 
     public static function get(){
 
@@ -33,17 +40,15 @@ class HeliumHire extends Product
             ->orderBy('productdetails.delivery_price')
             ->get();
 
-        return $cylinders->map(function ($cylinder, $key) {
+        $balloonDetails = collect(self::$balloonDetails);
 
-            $cylinder->balloonsCanFill = self::$balloonsCanFill[$key];
-            $cylinder->miniDescription = self::$miniDescriptions[$key];
+        return $cylinders->map(function ($cylinder, $key) use ($balloonDetails) {
 
-            // If the cylinder has two prices, select the lowest
-            if( $cylinder->ap_collect_price !== NULL && $cylinder->boc_collect_price !== NULL ){
-                $cylinder->lowestCollectPrice = min( [$cylinder->ap_collect_price, $cylinder->boc_collect_price] );
-            }else {
-                $cylinder->lowestCollectPrice = $cylinder->ap_collect_price ?? $cylinder->boc_collect_price;
-            }
+            $balloonDetail = $balloonDetails->firstWhere('id', $cylinder->id);
+
+            $cylinder->balloonsCanFill = $balloonDetail['balloonsCanFill'];
+            $cylinder->miniDescription = $balloonDetail['miniDesc'];
+            $cylinder->lowestCollectPrice = $cylinder->ap_collect_price;
 
             return $cylinder;
         });
